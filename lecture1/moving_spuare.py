0ec8c89d2d92acd91fb112d4599e2dcec35fd9bb
Muhammed Musab Kaya <musabk2002@gmail.com>
2025-11-18 04:30:10 +0300

    feat: Object movement system and solution for diagonal speed boost.

--- lecture1/moving_spuare.py
+++ lecture1/moving_spuare.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 
 # Initialize main event
 pygame.init()
@@ -8,6 +9,8 @@ square_x = 400
 square_y = 300
 square_size = 50
 square_color = (200, 30, 30)
+
+square_speed = 5
 
 # Set window size
 screen = pygame.display.set_mode((800, 600))
@@ -23,7 +26,34 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # 2) UPDATE (empty for now)
+    # 2) UPDATE
+    keys = pygame.key.get_pressed()
+
+    dx = 0
+    dy = 0
+
+    # 1) Set dx, dy for pressed button
+    if keys[pygame.K_RIGHT]:
+        dx += 1
+    if keys[pygame.K_LEFT]:
+        dx -= 1
+    if keys[pygame.K_UP]:
+        dy -= 1
+    if keys[pygame.K_DOWN]:
+        dy += 1
+    if (dx, dy) == (0, 0):
+        dx = 0
+        dy = 0
+
+    # 2) Normalize (dx, dy) if needed
+    if (dx, dy) != (0, 0):
+        length = math.sqrt(dx * dx + dy * dy)
+        dx = dx / length * square_speed
+        dy = dy / length * square_speed
+
+    # 3) Finally update square_x and square_y
+    square_x += dx
+    square_y += dy
 
     # 3) RENDER
     screen.fill((30, 30, 30)) # <-- Fills the entire window with dark greyish color
