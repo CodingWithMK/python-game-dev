200b9fe4ebf9725a0bbdbc26e9f01a2dfe29e540
Muhammed Musab Kaya <musabk2002@gmail.com>
2025-11-22 04:37:15 +0300

    feat: Lecture 5 - Velocity & Friction Handling for smooth Movement.

--- lecture1/moving_spuare.py
+++ lecture1/moving_spuare.py
@@ -10,6 +10,14 @@ square_y = 300
 square_size = 50
 square_color = (200, 30, 30)
 
+# Speed features
+MAX_SPEED = 300
+ACCELERATION = 800
+FRICTION = 1000
+
+velocity_x = 0
+velocity_y = 0
+
 square_speed = 300
 
 WIDTH = 800
@@ -35,8 +43,7 @@ while running:
     # 2) UPDATE
     keys = pygame.key.get_pressed()
 
-    dx = 0
-    dy = 0
+    dx, dy = 0, 0
 
     # 1) Set dx, dy for pressed button
     if keys[pygame.K_RIGHT]:
@@ -54,12 +61,36 @@ while running:
     # 2) Normalize (dx, dy) if needed
     if (dx, dy) != (0, 0):
         length = math.sqrt(dx * dx + dy * dy)
-        dx = dx / length * square_speed
-        dy = dy / length * square_speed
+        vel_x = dx / length
+        vel_y = dy / length
 
-    # 3) Finally update square_x and square_y with delta time
-    square_x += dx * delta_time
-    square_y += dy * delta_time
+        # 3) Update speed
+        velocity_x += dx * ACCELERATION * delta_time
+        velocity_y += dy * ACCELERATION * delta_time
+
+    else:
+        # ----- Apply friction if no input -----
+        if velocity_x != 0:
+            velocity_x -= (1 if velocity_x > 0 else -1) * FRICTION * delta_time
+            # threshold to prevent small oscillations
+            if abs(velocity_x) < 1e-2:
+                velocity_x = 0
+        
+        if velocity_y != 0:
+            velocity_y -= (1 if velocity_y > 0 else -1) * FRICTION * delta_time
+            if abs(velocity_y) < 1e-2:
+                velocity_y = 0
+
+    # ----- Setup velocity limit (MAX_SPEED) -----
+    # Find speed vector
+    speed = (velocity_x * velocity_x + velocity_y * velocity_y) ** 0.5
+    # Handle max_speed exceeding
+    if speed > MAX_SPEED:
+        velocity_x = (velocity_x / speed) * MAX_SPEED
+        velocity_y = (velocity_y / speed) * MAX_SPEED
+
+    square_x += velocity_x * delta_time
+    square_y += velocity_y * delta_time
 
     # 4) Boundary Clamp
     if square_x < 0:
